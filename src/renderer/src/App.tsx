e03307bd58d874af4b507fcaba59ee581c999f97
florianmg <mesagouiffes@gmail.com>
2024-02-10 15:39:39 +0100

    initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,7 @@
+function App(): JSX.Element {
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return <p>edit file this file /src/renderer/src/App.tsx</p>
+}
+
+export default App
